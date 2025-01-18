16b9b5ebaed573b10bfc08f7f27ad4b91a913245
Firdavs Yorkulov <113686185+FirdavsCoder@users.noreply.github.com>
2025-01-18 23:04:42 +0500

    updated

--- handlers/channels/edit_kb.py
+++ handlers/channels/edit_kb.py
@@ -1,10 +1,11 @@
 from aiogram import types
 
+from handlers.admin.key import adminKeyboard
 from loader import dp, db, bot
 
 @dp.channel_post_handler(state=None)
 async def channel_post(message: types.Message):
     if message.chat.type == 'channel':
-        await message.edit_reply_markup()
+        await message.edit_reply_markup(reply_markup=await adminKeyboard.buttons_edit())
     else:
         await message.answer("Bu kanalda post qo'shish mumkin emas!")
